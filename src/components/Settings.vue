efec1dd6559519e544dcff360a5b6743d2b55605
user100 <user100@example.com>
2023-02-12 02:46:18 +0200

    Removed the alpha vantage API update field

--- src/components/Settings.vue
+++ src/components/Settings.vue
@@ -11,18 +11,7 @@
             <div class="col-auto">
                 <button v-on:click="updateYKey" v-bind:disabled="yKeyButtonDisabled" type="button" class="btn btn-success">Update</button>
             </div>
-        </form>
-        <form class="row py-2 gy-2 gx-3 align-items-center">
-            <div class="col-auto">
-                <label for="inputAlphaKey">Alpha API Key:</label>
-            </div>
-            <div class="col-auto">
-                <input v-model="akey" type="password" class="form-control" id="inputAlphaKey">
-            </div>
-            <div class="col-auto">
-                <button v-on:click="updateAKey" v-bind:disabled="aKeyButtonDisabled" type="button" class="btn btn-success">Update</button>
-            </div>
-        </form>      
+        </form>    
     </div>
 </template>
 
@@ -33,9 +22,7 @@ import axios from 'axios'
         data() {
         return {
                 ykey: "",
-                akey: "",
                 yKeyButtonDisabled: true,
-                aKeyButtonDisabled: true,
                 resp: {}
             }
         },
@@ -46,13 +33,6 @@ import axios from 'axios'
                 } else {
                     this.yKeyButtonDisabled= false
                 }
-            },
-            akey: function() {
-                if(this.akey === "") {
-                    this.aKeyButtonDisabled = true
-                } else {
-                    this.aKeyButtonDisabled= false
-                }
             }
         },
         methods: {
@@ -60,25 +40,6 @@ import axios from 'axios'
                 const tempkey = this.ykey
                 this.ykey = ""
                 await axios.put(`${process.env.VUE_APP_STOCKSCLIENTAPI2}/keys/yahoo`, {key: tempkey}, {withCredentials: true})
-                    .then(response => (this.resp = response.data))
-                    .catch(function(error){this.$toast.error(error)});
-                if(this.resp.success === false)
-                {
-                    this.$toast.error(this.resp.data, {
-                    dismissible: true
-                    });
-                }
-                else
-                {
-                    this.$toast.success(this.resp.data, {
-                    dismissible: true
-                    });
-                }
-            },
-            updateAKey: async function(){
-                const tempkey = this.akey
-                this.akey = ""
-                await axios.put(`${process.env.VUE_APP_STOCKSCLIENTAPI2}/keys/alpha`, {key: tempkey}, {withCredentials: true})
                     .then(response => (this.resp = response.data))
                     .catch(function(error){this.$toast.error(error)});
                 if(this.resp.success === false)
