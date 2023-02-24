ec7a1850726de8e4eef7dddcf86732a5f1a69fb2
user100 <user100@example.com>
2023-02-24 22:16:55 +0200

    Added listing of competitors

--- src/components/StockOverview.vue
+++ src/components/StockOverview.vue
@@ -4,7 +4,7 @@
             <div class="col-12">
                 <h5>Competitors</h5>
                 <span v-for="competitor in stock.competitors" v-bind:key="competitor">
-                    <span class="pr-4">{{ competitor }}</span>
+                    <span class="mr-2">{{ competitor }}</span>
                 </span>
             </div>
         </div>
